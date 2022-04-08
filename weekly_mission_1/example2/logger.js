f0808159ff9725bfb81bd6c0fd91bc393941dcd2
Hector Ramirez <galle43@gmail.com>
2022-04-08 17:46:43 -0600

    added examples

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,13 @@
+// logger.js
+
+exports.info = (message) => {
+  console.log(`info: ${message}`);
+};
+
+exports.verbose = (message) => {
+  console.log(`verbose: ${message}`);
+};
+
+/*
+    const logger = require ('./logger')
+    */
---
+++ weekly_mission_1/example3/main.js
@@ -0,0 +1,5 @@
+const logger1 = require("./logger_1");
+const logger2 = require("./logger_2");
+
+logger1("This an informational message");
+logger2.verbose("this is a verbose message");
