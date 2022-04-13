186e37df46a1efc3d2450c4cefafd89fc25e9632
Hector Ramirez <galle43@gmail.com>
2022-04-13 11:44:51 -0600

    add example 6 - 10

---
+++ weekly_mission_2/hello/hello.js
@@ -0,0 +1,22 @@
+//Concatenate
+let text = "con" + "ca" + "te" + "nate";
+console.log(text);
+
+//Template literals
+console.log(`half of 100 is ${100 / 2}`);
+
+//Unary operators
+console.log(typeof 4.5);
+console.log(typeof "x");
+
+console.log(-(10 - 2));
+
+//boolean
+console.log(3 > 2);
+console.log(2 > 3);
+
+const square = (x) => {
+  return x * x;
+};
+
+console.log(square(2));
